579f190666bf71e13990480d57dbd97d81fa96fb
psyoon12 <psyoon12a@gmail.com>
2022-05-11 00:39:11 -0400

    Volume Setting

---
+++ Serious_Game/Assets/Sctipts/BackgroundMusic.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    public static BackgroundMusic instance;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else{
+            Destroy(gameObject);
+        }
+    }
+}
--- Serious_Game/Assets/Sctipts/SettingsMenu.cs
+++ Serious_Game/Assets/Sctipts/SettingsMenu.cs
@@ -2,16 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
-
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
-
-    public AudioMixer audioMixer;
-
-    public void SetVolume (float volume) 
+    //public AudioMixer audioMixer;
+    [SerializeField] Slider volumeSlider;
+    /*
+    public void SetVolume(float volume)
     {
-        audioMixer.SetFloat("volume", volume);
-    } 
+        audioMixer.SetFloat("volume", Mathf.Log10(volume) * 20);
+    }
+    */
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("volume"))
+        {
+            PlayerPrefs.SetFloat("volume", 1);
+            Load();
+        }
+        else{
+            Load();
+        }
+    }
 
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+        Save();
+    }
+
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("volume");
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("volume", volumeSlider.value);
+    }
 }
